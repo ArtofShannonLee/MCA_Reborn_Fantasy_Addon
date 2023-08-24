599d1285560e50b1348ea429ae0cbdf801c9dfc5
ArtofShannonLee <124021450+ArtofShannonLee@users.noreply.github.com>
2023-08-24 16:56:54 +1000

    MCAR dependencies successfully installed

--- src/main/java/com/artofshannonlee/fantasymcar/FantasyMCAR.java
+++ src/main/java/com/artofshannonlee/fantasymcar/FantasyMCAR.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -14,6 +15,7 @@ import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import software.bernie.geckolib3.GeckoLib;
 
 import java.util.stream.Collectors;
 
@@ -26,8 +28,9 @@ public class FantasyMCAR
     private static final Logger LOGGER = LogManager.getLogger();
 
     public FantasyMCAR() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        GeckoLib.initialize();
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
