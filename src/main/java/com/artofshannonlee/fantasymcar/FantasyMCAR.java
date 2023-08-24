ab32587f417e17b442cb3c83ee4021b0cfa15940
ArtofShannonLee <124021450+ArtofShannonLee@users.noreply.github.com>
2023-08-24 13:53:09 +1000

    Mappings

--- src/main/java/com/artofshannonlee/fantasymcar/FantasyMCAR.java
+++ src/main/java/com/artofshannonlee/fantasymcar/FantasyMCAR.java
@@ -17,9 +17,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(FantasyMCAR.MOD_ID)
-public class FantasyMCAR //ferret
+public class FantasyMCAR
 {
     public static final String MOD_ID = "fantasymcar";
 
