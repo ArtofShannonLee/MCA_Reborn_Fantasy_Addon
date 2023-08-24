5f5600afbfa15d1e79ef16a00afd6f15f32f76bb
ArtofShannonLee <124021450+ArtofShannonLee@users.noreply.github.com>
2023-08-24 13:27:07 +1000

    added comment test

--- src/main/java/com/artofshannonlee/fantasymcar/FantasyMCAR.java
+++ src/main/java/com/artofshannonlee/fantasymcar/FantasyMCAR.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(FantasyMCAR.MOD_ID)
-public class FantasyMCAR
+public class FantasyMCAR //ferret
 {
     public static final String MOD_ID = "fantasymcar";
 
